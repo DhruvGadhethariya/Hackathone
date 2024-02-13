58a76f59822565ca8d8e7e0cd8b775e512534ebf
DhruvGadhethariya <153472326+DhruvGadhethariya@users.noreply.github.com>
2024-02-13 10:53:05 +0530

    new

---
+++ Task/Areas/Acc_AccountDocument/Controllers/Acc_AccountDocumentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task.Areas.Acc_AccountDocument.Controllers
+{
+    public class Acc_AccountDocumentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
